a7a77ee5769aa965319ba6feebd8004acdc69c58
FatmaRida <53396911+FatmaRida@users.noreply.github.com>
2021-01-15 02:51:12 +0200

    ARL is a website for iso certification
    ARL is a website for iso certification

---
+++ js/policy.js
@@ -0,0 +1,43 @@
+
+
+// navbar js
+(function($){
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+      if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+      }
+      var $subMenu = $(this).next(".dropdown-menu");
+      $subMenu.toggleClass('show');
+
+      $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass("show");
+      });
+
+      return false;
+    });
+  })(jQuery);
+
+
+  // end navbar
+ new WOW().init();
+
+// button toggle
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+// button toggle div3
+function myFunction() {
+  var x = document.getElementById("myDIV3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
